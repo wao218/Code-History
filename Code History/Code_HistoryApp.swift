d4e6dd7608d193598f0805979507bd592e10244c
wao218 <wesleyosborne1@gmail.com>
2023-10-13 19:13:36 -0700

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by wesley osborne on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by wesley osborne on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
