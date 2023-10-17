35a02e2018a105ce439583cf06b9cd4d5546aeb5
wao218 <wesleyosborne1@gmail.com>
2023-10-16 19:21:13 -0700

    Refactor HStack to use ForEach to display buttons

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         correctAnswerIndex: 2
     )
     
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     
     var body: some View {
         ZStack {
@@ -30,28 +30,14 @@ struct ContentView: View {
                     .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                 Spacer()
                 HStack {
-                    Button(action: {
-                        print("Tapped on Choice 1")
-                    }, label: {
-                        ChoiceTextView(choiceText:question.possibleAnswers[0])
-                            
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 2")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                            
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 3")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 4")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-                    })
+                    ForEach(0..<question.possibleAnswers.count, id: \.self) { answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                    }
                 }
             }
         }.foregroundStyle(.white)
