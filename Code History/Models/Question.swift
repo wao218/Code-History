8f625ed8898b0071c7c729d22766ce202ec0e572
wao218 <wesleyosborne1@gmail.com>
2023-10-17 20:59:09 -0700

    Add Game Model and organize files into MVVM

---
+++ Code History/Models/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by wesley osborne on 10/16/23.
+//
+
+import Foundation
+
+struct Question: Hashable {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(
+            questionText: "Who invented the World Wide Web?",
+            possibleAnswers: [
+                "Steve Jobs",
+                "Linus Torvalds",
+                "Bill Gates",
+                "Tim Berners-Lee"
+            ],
+            correctAnswerIndex: 3),
+        Question(
+            questionText: "What was the first object oriented programming language?",
+            possibleAnswers: [
+                "Simula",
+                "Python",
+                "Swift",
+                "C"
+            ],
+            correctAnswerIndex: 0)
+    ]
+}
